75d815ed38753eb4c34780dd8b2707daaf9d6a68
shubuzuo <shubuzuo@gmail.com>
2026-02-05 23:37:06 +0800

    chore(lint-staged): 添加 cspell 检查配置

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 让 Prettier 自己决定处理什么文件 (利用 --ignore-unknown)
-  '*': 'prettier --write --ignore-unknown',
+  '*': ['prettier --write --ignore-unknown', 'cspell --no-progress'],
   // ESLint 仍然建议限制扩展名，防止传图片/二进制给它导致报错
   '*.{js,ts,jsx,tsx,mjs,cjs}': 'eslint --fix',
   // 对 TS 文件进行类型检查，且忽略文件名参数（全量检查）
